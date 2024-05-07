5d32c07dabbea8c0e871085a3b818ed42b5c345e
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-05-07 21:08:42 +0530

    Create EvenOrOdd.py

---
+++ Basic_question/EvenOrOdd.py
@@ -0,0 +1,8 @@
+def check_odd_even(number):
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+num = int(input("Enter a number: "))
+check_odd_even(num)
