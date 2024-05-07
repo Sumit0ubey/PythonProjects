28b0384126ff15f722d23ae05793b71c6f3f15b9
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-05-07 20:39:37 +0530

    Create DrawSquareDot.py

---
+++ GUI_Projects/TurtleModule_Projects/DrawSquareDot.py
@@ -0,0 +1,13 @@
+import turtle
+
+# Create a Turtle screen
+screen = turtle.Screen()
+
+# Create a Turtle object
+t = turtle.Turtle()
+
+# Draw a dot
+t.dot(50, "blue")
+
+# Keep the screen open until the user closes it
+screen.mainloop()
