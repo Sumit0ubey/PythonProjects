e1f97b0b16ce2bcad1878051d1123321d1d67d7a
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-05-07 20:46:49 +0530

    Create SumValues_Dic.py

---
+++ On_CollectionDatatypes/SumValues_Dic.py
@@ -0,0 +1,7 @@
+def sum_dictionary_items(dictionary):
+    return sum(dictionary.values())
+
+# Test the function
+my_dict = {'a': 10, 'b': 20, 'c': 30}
+total_sum = sum_dictionary_items(my_dict)
+print("Sum of all items in dictionary:", total_sum)
