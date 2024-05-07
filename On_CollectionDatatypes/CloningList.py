fd9b0127bab61bcdba0ac78b44e845b0cd479a07
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-05-07 20:43:29 +0530

    Create CloningList.py

---
+++ On_CollectionDatatypes/CloningList.py
@@ -0,0 +1,8 @@
+def clone_list(lst):
+    return lst[:]
+
+# Test the function
+original_list = [1, 2, 3, 4, 5]
+copied_list = clone_list(original_list)
+print("Original list:", original_list)
+print("Copied list:", copied_list)
