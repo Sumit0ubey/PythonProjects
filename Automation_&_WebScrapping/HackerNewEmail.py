f68bd96298aefffc05476522eee1e7d573354360
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2025-04-04 13:33:30 +0530

    Update HackerNewEmail.py

--- Automation_&_WebScrapping/HackerNewEmail.py
+++ Automation_&_WebScrapping/HackerNewEmail.py
@@ -1,77 +1,105 @@
-import os
-import requests
-import smtplib
 from bs4 import BeautifulSoup
+from requests import request
+
+from os import getenv
+from datetime import datetime
 from dotenv import load_dotenv
-from email.mime.text import MIMEText
+from smtplib import SMTP, SMTPException
 from email.mime.multipart import MIMEMultipart
-import datetime as dt
+from email.mime.text import MIMEText
 
 load_dotenv()
-email1 = os.getenv('FROMEMAIL')
-email2 = os.getenv('TOEMAIL')
-password = os.getenv('PASSWORD')
-
 SERVER = 'smtp.gmail.com'
 PORT = 587
-FROM = email1
-TO = 'dubeysumit378@gmail.com'
-PASS = password
+FROM = getenv('FROMEMAIL')
+TO = getenv('RECIPIENTS').split(",")
+PASS = getenv('PASSWORD')
 
-def extract_news(url: str) -> str:
-    Emailcontent = ''
-    Emailcontent += '<b> HN Top Stories: </b>\n' + '<br>' + '-'*50 + '<br>'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.content
-        Beautiful = BeautifulSoup(data, 'html.parser')
-        
-        for i, topic in enumerate(Beautiful.find_all('td', attrs={'class': 'title', 'valign': ''})):
-            if topic.text.lower() != 'more':
-                Emailcontent += f'{str(i + 1)}: {topic.text.strip()}<br>'
-        
-    except requests.exceptions.RequestException as e:
-        Emailcontent += f'Error fetching news: {str(e)}'
-    return Emailcontent
+def extractNews(url: str) -> str:
+    response = request(url=url, method="GET")
+    data = response.content
+    soup = BeautifulSoup(data, "html.parser")
+
+    content = """
+    <html>
+    <body style="margin:0; padding:0; font-family:Arial, sans-serif; background-color:#28282B;">
+        <table width="100%" cellpadding="0" cellspacing="0" style="max-width:600px; margin:auto; background-color:#ffffff; padding:20px; border-radius:8px; box-shadow:0px 4px 10px rgba(0,0,0,0.1);">
+        <tr>
+            <td align="center">
+            <h1 style="color:#333;">🚀 Daily IT & Tech News</h1>
+            <p style="font-size:14px; color:#777;">Bringing you the latest tech updates every day.</p>
+            <hr style="border:1px solid #ddd; width:90%;">
+            </td>
+        </tr>
+    """
+
+    for i, title in enumerate(soup.find_all("h3", class_="loop-card__title")):
+        url = title.find("a")
+        content += f"""
+        <tr>
+            <td style="padding:15px; border-bottom:1px solid #ddd; display:flex; align-items:center;">
+            <div>
+                <p style="margin:0;">
+                <a style="text-decoration: none; color: black;" href="{url['href']}" style="color:#007BFF; text-decoration:none;">
+                    📌 {title.text}
+                </a>
+                </p>
+            </div>
+            </td>
+        </tr>
+        """
+
+    content += """
+                <tr>
+                    <td align="center" style="padding-top:30px;">
+                    <p style="font-size:12px; color:#999;">You are receiving this email because you subscribed to our daily tech news.</p>
+                    <p style="font-size:12px; color:#999;">Powered by <strong>Sumit Dubey</strong></p>
+                    <a href="https://github.com/Sumit0ubey/" 
+                        style="font-size:12px; color:#FF0000; text-decoration:none;">Unsubscribe</a>
+                    </td>
+                </tr> 
+            </table>
+        </body>
+    </html>"""
+
+    return content
+
 
 def setServer():
     try:
-        server = smtplib.SMTP(SERVER, PORT)
-        server.set_debuglevel(1)
-        server.ehlo()
+        server = SMTP(SERVER, PORT)
         server.starttls()
+        server.ehlo()
         server.login(FROM, PASS)
         return server
-    except smtplib.SMTPException as e:
-        print(f"Error setting up the email server: {str(e)}")
+    except SMTPException as e:
+        print(f"An Error Occur while connecting to the server: {e}")
         raise
 
-def CreateMail(data: str, server):
-    now = dt.datetime.now()
-    content = f'{data}<br>-------------<br><br>End of Message'
-
-    Message = MIMEMultipart()
-    Message['Subject'] = f"Top News Stories HN - {str(now.day)} {str(now.year)}"
-    Message['From'] = FROM
-    Message['To'] = TO
-
-    Message.attach(MIMEText(content, 'html'))
-    try:
-        server.sendmail(FROM, TO, Message.as_string())
-        print("Email Sent")
-    except smtplib.SMTPException as e:
-        print(f"Error sending email: {str(e)}")
-
+def createMail(data: str, server):
+    date = datetime.now()
+    
+    for user in TO:
+        message = MIMEMultipart()
+        message["From"] = FROM
+        message["Subject"] = f"IT News - Automation Email | {str(date.day)}:{str(date.month)}:{str(date.year)}"
+        message.attach(MIMEText(data, "html"))
+        message["To"] = user
+        try:
+            server.sendmail(FROM, user, message.as_string())
+        except SMTPException as e:
+            print(f"Error sending email: {str(e)}")
+        print(f"Email send to {user}")
 
 def main():
     print("Extracting News Stories.......")
-    data = extract_news('https://news.ycombinator.com/')
+    data = extractNews('https://techcrunch.com/latest/')
     print("Initiating Server......")
     server = setServer()
     print("Composing Email......")
-    CreateMail(data=data, server=server)
+    createMail(data=data, server=server)
     server.quit()
+    print("Email Send")
 
 if __name__ == '__main__':
     main()
